c8ce463d25dbebd925af64b265c38926b8fd4f1e
Bartłomiej Klimczak <bartlomiej.klimczak88@gmail.com>
2021-12-05 15:23:11 +0100

    feat: perf optimization

--- 2021/day5/day5.go
+++ 2021/day5/day5.go
@@ -22,99 +22,62 @@ func main() {
 }
 
 func partA(input string, matrix [][]int) int {
-	for _, line := range strings.Split(input, "\n") {
-		l := strings.Split(line, " -> ")
-		l1 := strings.Split(l[0], ",")
-		x1 := strToInt(l1[0])
-		y1 := strToInt(l1[1])
-
-		l2 := strings.Split(l[1], ",")
-		x2 := strToInt(l2[0])
-		y2 := strToInt(l2[1])
-
-		calcPaths(matrix, x1, x2, y1, y2)
-	}
-
-	return sumMatrix(matrix)
-}
-
-func partB(input string, matrix [][]int) int {
-	for _, line := range strings.Split(input, "\n") {
-		l := strings.Split(line, " -> ")
-		l1 := strings.Split(l[0], ",")
-		x1 := strToInt(l1[0])
-		y1 := strToInt(l1[1])
-
-		l2 := strings.Split(l[1], ",")
-		x2 := strToInt(l2[0])
-		y2 := strToInt(l2[1])
-
-		calcPaths2(matrix, x1, x2, y1, y2)
-		// printMatrix(matrix)
-	}
-
-	return sumMatrix(matrix)
-}
-
-func sumMatrix(matrix [][]int) int {
 	sum := 0
-	for x := range matrix {
-		for y := range matrix[x] {
-			if matrix[x][y] > 1 {
-				sum++
-			}
-		}
+	for _, line := range strings.Split(input, "\n") {
+		l := strings.Split(line, " -> ")
+		l1 := strings.Split(l[0], ",")
+		x1 := strToInt(l1[0])
+		y1 := strToInt(l1[1])
+
+		l2 := strings.Split(l[1], ",")
+		x2 := strToInt(l2[0])
+		y2 := strToInt(l2[1])
+
+		sum += calcPaths(matrix, x1, x2, y1, y2)
 	}
 
 	return sum
 }
 
-func calcPaths(matrix [][]int, x1, x2, y1, y2 int) {
-	// horizontal
-	if x1 == x2 {
-		if y1 > y2 {
-			for y := y1; y != (y2 - 1); y += -1 {
-				matrix[x1][y]++
-			}
-		} else {
-			for y := y1; y != (y2 + 1); y++ {
-				matrix[x1][y]++
-			}
-		}
+func partB(input string, matrix [][]int) int {
+	sum := 0
+	for _, line := range strings.Split(input, "\n") {
+		l := strings.Split(line, " -> ")
+		l1 := strings.Split(l[0], ",")
+		x1 := strToInt(l1[0])
+		y1 := strToInt(l1[1])
 
-		return
+		l2 := strings.Split(l[1], ",")
+		x2 := strToInt(l2[0])
+		y2 := strToInt(l2[1])
+
+		sum += calcPaths2(matrix, x1, x2, y1, y2)
 	}
 
-	// vertical
-	if y1 == y2 {
-		if x1 > x2 {
-			for x := x1; x != (x2 - 1); x += -1 {
-				matrix[x][y1]++
-			}
-		} else {
-			for x := x1; x != (x2 + 1); x++ {
-				matrix[x][y1]++
-			}
-		}
-
-		return
-	}
+	return sum
 }
 
-func calcPaths2(matrix [][]int, x1, x2, y1, y2 int) {
+func calcPaths(matrix [][]int, x1, x2, y1, y2 int) int {
+	c := 0
 	// horizontal
 	if x1 == x2 {
 		if y1 > y2 {
 			for y := y1; y != (y2 - 1); y += -1 {
 				matrix[x1][y]++
+				if matrix[x1][y] == 2 {
+					c++
+				}
 			}
 		} else {
 			for y := y1; y != (y2 + 1); y++ {
 				matrix[x1][y]++
+				if matrix[x1][y] == 2 {
+					c++
+				}
 			}
 		}
 
-		return
+		return c
 	}
 
 	// vertical
@@ -122,14 +85,65 @@ func calcPaths2(matrix [][]int, x1, x2, y1, y2 int) {
 		if x1 > x2 {
 			for x := x1; x != (x2 - 1); x += -1 {
 				matrix[x][y1]++
+				if matrix[x][y1] == 2 {
+					c++
+				}
 			}
 		} else {
 			for x := x1; x != (x2 + 1); x++ {
 				matrix[x][y1]++
+				if matrix[x][y1] == 2 {
+					c++
+				}
+			}
+		}
+	}
+
+	return c
+}
+
+func calcPaths2(matrix [][]int, x1, x2, y1, y2 int) int {
+	c := 0
+	// horizontal
+	if x1 == x2 {
+		if y1 > y2 {
+			for y := y1; y != (y2 - 1); y += -1 {
+				matrix[x1][y]++
+				if matrix[x1][y] == 2 {
+					c++
+				}
+			}
+		} else {
+			for y := y1; y != (y2 + 1); y++ {
+				matrix[x1][y]++
+				if matrix[x1][y] == 2 {
+					c++
+				}
 			}
 		}
 
-		return
+		return c
+	}
+
+	// vertical
+	if y1 == y2 {
+		if x1 > x2 {
+			for x := x1; x != (x2 - 1); x += -1 {
+				matrix[x][y1]++
+				if matrix[x][y1] == 2 {
+					c++
+				}
+			}
+		} else {
+			for x := x1; x != (x2 + 1); x++ {
+				matrix[x][y1]++
+				if matrix[x][y1] == 2 {
+					c++
+				}
+			}
+		}
+
+		return c
 	}
 
 	// diagonal
@@ -140,24 +154,38 @@ func calcPaths2(matrix [][]int, x1, x2, y1, y2 int) {
 			if y1 > y2 {
 				for i := 0; i <= diff; i++ {
 					matrix[x1-i][y1-i]++
+					if matrix[x1-i][y1-i] == 2 {
+						c++
+					}
 				}
 			} else {
 				for i := 0; i <= diff; i++ {
 					matrix[x1-i][y1+i]++
+					if matrix[x1-i][y1+i] == 2 {
+						c++
+					}
 				}
 			}
 		} else {
 			if y1 > y2 {
 				for i := 0; i <= diff; i++ {
 					matrix[x1+i][y1-i]++
+					if matrix[x1+i][y1-i] == 2 {
+						c++
+					}
 				}
 			} else {
 				for i := 0; i <= diff; i++ {
 					matrix[x1+i][y1+i]++
+					if matrix[x1+i][y1+i] == 2 {
+						c++
+					}
 				}
 			}
 		}
 	}
+
+	return c
 }
 
 func abs(a int) int {
@@ -171,15 +199,4 @@ func abs(a int) int {
 func strToInt(s string) int {
 	i, _ := strconv.Atoi(s)
 	return i
-}
-
-func printMatrix(m [][]int) {
-	for x := range m {
-		for y := range m[x] {
-			fmt.Printf("%d ", m[x][y])
-		}
-		fmt.Println()
-	}
-
-	fmt.Println()
 }
